74aa90ce9c7826810cd79ebdd08bd55b6b90559d
seahpark247 <famousjedo@gmail.com>
2025-03-03 07:34:51 -0600

    Initial Commit

---
+++ BetterRest/BetterRestApp.swift
@@ -0,0 +1,17 @@
+//
+//  BetterRestApp.swift
+//  BetterRest
+//
+//  Created by Seah Park on 3/3/25.
+//
+
+import SwiftUI
+
+@main
+struct BetterRestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
